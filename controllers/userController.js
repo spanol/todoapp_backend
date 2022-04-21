5fa8bd7beef1dcc6781c3a1e02948d71db321be5
spanol <viniciusspanol@gmail.com>
2022-04-21 01:41:40 -0300

    commit
    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,6 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please add all fields");
   }
 
-  // Check if user exists
   const userExists = await User.findOne({ username });
 
   if (userExists) {
@@ -19,11 +18,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
-  // Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  // Create user
   const user = await User.create({
     username,
     password: hashedPassword,
@@ -44,7 +41,6 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
     const { username, password } = req.body
   
-    // Check for user email
     const user = await User.findOne({ username })
   
     if (user && (await bcrypt.compare(password, user.password))) {
@@ -63,7 +59,6 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(200).json(req.username)
   })
   
-  // Generate JWT
   const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
       expiresIn: '30d',
